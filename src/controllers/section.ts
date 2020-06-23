44b1354df192c7ae962331ce2ba69c4c70af72d7
Evan Ruby <evanruby@bedrockdetroit.com>
2020-06-23 14:20:25 -0400

    Stub out updateSection, remove unnecessary comments

--- src/controllers/section.ts
+++ src/controllers/section.ts
@@ -2,7 +2,6 @@ import { RequestHandler } from 'express'
 import createError from 'http-errors'
 import Section from '../models/section'
 
-// GET /sections?seriesId=0 (Returns Array of Sections - time allocated, name, items)
 export const getSections: RequestHandler = (req, res, next) => {
   const { seriesId } = req.query
   if (!seriesId) {
@@ -13,4 +12,13 @@ export const getSections: RequestHandler = (req, res, next) => {
   const seriesSections: Section[] = []
   return res.json(seriesSections)
 }
-// PUT /sections/:sectionId (Update the section - return 200 on success)
+
+export const updateSection: RequestHandler = (req, res, next) => {
+  const sectionId: string = req.params.id
+  // TODO - Given the sectionId, update the section in the database with that id
+  const dbErr = false
+  if (dbErr) {
+    return next(createError(404, `No section with id ${sectionId} was found`))
+  }
+  return res.sendStatus(204)
+}
